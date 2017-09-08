82554d30ebfe62b5852a51e594fd6af8507283e5
github username <github email address>
2017-09-08 11:11:49 -0400

    pencils down

--- author.rb
+++ author.rb
@@ -1,1 +1,30 @@
+class Author
+  attr_accessor :books, :name, :total_words
 
+  ALL = []
+
+  def self.all
+    ALL
+  end
+
+  def self.most_words
+    sorted = self.all.sort_by do |author|
+      author.total_words
+    end
+    sorted[0]
+  end
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @total_words = 0
+    ALL << self
+  end
+
+  def write_book(title, word_count)
+    book = Book.new(title, word_count, self)
+  end
+
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,23 @@
+class Book
+  attr_accessor :title, :word_count, :author
 
+  ALL = []
+
+  def self.all
+    ALL
+  end
+
+  def initialize(title, word_count, author)
+    @title = title
+    @word_count = word_count
+    @author = author
+    author.books << self
+    author.total_words += word_count
+    ALL << self
+  end
+
+
+
+
+
+end
